347c3378c452f744bb4d15452526ef858fa79fec
Scott Wells <scottbwells@gmail.com>
2015-04-06 15:41:11 -0600

    spreadsheet first

---
+++ spreadsheet_server.h
@@ -0,0 +1,23 @@
+// SpreadsheetServer.h
+#ifndef SPREADSHEET_SERVER_H
+#define SPREADSHEET_SERVER_H
+
+#include <string>
+
+class spreadsheet_server
+{
+public:
+  spreadsheet_server();
+  listen();
+  connect();
+  message_received();
+  send_message(std::string);
+
+  ~huge_number();
+
+private:
+}
+
+
+
+# endif
